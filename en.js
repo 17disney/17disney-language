2202984e98b9f6c0ccae90c8fb4e4ce6d019d52b
xank <xank@qq.com>
2018-06-07 22:12:20 +0800

    .

---
+++ en.js
@@ -0,0 +1,31 @@
+export default {
+  ds: {
+    label: {
+      name: 'Name',
+      status: 'Status',
+      waitsAvg: 'WaitAvg',
+      trend: 'Trend'
+    },
+    status: {
+      operating: 'Operating',
+      closed: 'Closed',
+      renewal: 'Renewal',
+      refurbishment: 'Refurbishment',
+      down: 'Down'
+    },
+    disneyLand: {
+      shanghai: 'Sahanghai Disneyland',
+      hongkong: 'Hongkong Disneyland',
+      california: 'California Disneyland',
+      paris: 'Paris Disneyland',
+      orlando: 'Orlando DisneyWord'
+    },
+    navbar: {
+      dashboard: 'Dashboard',
+      explorer: 'Explorer',
+      waitTimes: 'WaitTimes',
+      timesGuide: 'TimesGuide',
+      flowOperia: 'FlowOperia'
+    }
+  }
+}
---
+++ zh-hans.js
@@ -0,0 +1,31 @@
+export default {
+  ds: {
+    label: {
+      name: '名称',
+      status: '状态',
+      waitsAvg: '平均等候',
+      trend: '趋势'
+    },
+    status: {
+      operating: '开放',
+      closed: '关闭',
+      renewal: '计划性维护',
+      refurbishment: '计划性维护',
+      down: '暂时关闭'
+    },
+    disneyLand: {
+      shanghai: '上海迪士尼乐园',
+      hongkong: '香港迪士尼乐园',
+      california: '加州迪士尼乐园',
+      paris: '巴黎迪士尼乐园',
+      orlando: '奥兰多迪士尼世界'
+    },
+    navbar: {
+      dashboard: '仪表盘',
+      explorer: '资源管理',
+      waitTimes: '等候时间',
+      timesGuide: '乐园时间表',
+      flowOperia: '客流预测'
+    }
+  }
+}
